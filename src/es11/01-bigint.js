695709b3ce6e81ca3c32bb1820410d2f85ea8005
codepalacios <chrispalacios95@gmail.com>
2023-02-22 21:29:27 -0600

    Push del código de práctica realizado en el curso

---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 123456789123456789n;
+const anotherBigNumber = BigInt(123456789123456789);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,2 @@
+const value = 100_000_00_000;
+console.log(value);
